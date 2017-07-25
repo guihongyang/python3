da3f3853b05368fe2129201d3068b759012129d3
guihongyang <262926731@qq.com>
2017-07-25 15:47:50 +0800

    python文件上传

---
+++ string.py
@@ -0,0 +1,10 @@
+str="中华人民共和国"
+print (str)
+print (str[0:1])	#截取1个字符
+print (str[0:-1])	#截取第一个到倒数第一个
+print (str[2])		#截取第n+1个字符
+print (str[2:])		#截取第n+1个到最后的字符串
+print (str*2)		#输出n次str字符串
+print ("ru\noob")
+print (r"ru\noob")
+print ("I "+"love "+"python")
---
+++ test.py
@@ -0,0 +1,4 @@
+a=b=c=10
+print (c,end=' ')
+d,e=21,22
+print (d)
